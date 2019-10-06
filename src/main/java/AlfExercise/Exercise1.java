247e247eebdb6d7a90c3d7e43719299a5ee35be5
Simona Carp <simo_carp@yahoo.com>
2019-10-06 17:34:28 +0300

    Added report at the end of the output

--- src/main/java/AlfExercise/Exercise1.java
+++ src/main/java/AlfExercise/Exercise1.java
@@ -7,21 +7,38 @@ public class Exercise1 {
     }
 
     private static void printFizzBuzzWithFor(int n) {
+        int integerCount = 0;
+        int alfrescoCount = 0;
+        int fizzbuzzCount = 0;
+        int buzzCount = 0;
+        int fizzCount = 0;
         for(int i=1; i<=n; i++){
             if(Integer.toString(i).contains("3")){
                 System.out.print("alfresco ");
+                alfrescoCount++;
             }
             else if(i%3==0 & i%5==0) {
                 System.out.print("fizzbuzz ");
+                fizzbuzzCount++;
             }
             else if(i%5==0){
                 System.out.print("buzz ");
+                buzzCount++;
             }
             else if(i%3==0){
                 System.out.print("fizz ");
+                fizzCount++;
             }
-            else System.out.print(i+ " ");
+            else {
+                System.out.print(i + " ");
+                integerCount++;
+            }
         }
+        System.out.println();
+        System.out.println("fizz " + fizzCount + ", buzz " + buzzCount + ", fizzbuzz "
+                + fizzbuzzCount + ", alfresco " + alfrescoCount
+                + ", integer " + integerCount
+        );
     }
 
 }
