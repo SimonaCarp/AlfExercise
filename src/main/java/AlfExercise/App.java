90aab5f833ef60d30079daf52cd888ddda18468a
Simona Carp <simo_carp@yahoo.com>
2019-10-06 17:03:45 +0300

    Added functionality for Step1

---
+++ src/main/java/AlfExercise/App.java
@@ -0,0 +1,13 @@
+package AlfExercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/AlfExercise/Exercise1.java
@@ -0,0 +1,24 @@
+package AlfExercise;
+
+public class Exercise1 {
+    public static void main(String [] args){
+        int n = 20;
+        printFizzBuzzWithFor(n);
+    }
+
+    private static void printFizzBuzzWithFor(int n) {
+        for(int i=1; i<=n; i++){
+            if(i%3==0 & i%5==0) {
+                System.out.print("fizzbuzz ");
+            }
+            else if(i%5==0){
+                System.out.print("buzz ");
+            }
+            else if(i%3==0){
+                System.out.print("fizz ");
+            }
+            else System.out.print(i+ " ");
+        }
+    }
+
+}
